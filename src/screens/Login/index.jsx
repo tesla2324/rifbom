a7d422f28bfb461388768ff37b3e916a922d0f73
fanny2203 <fanny.cryf.meddwl@gmail.com>
2023-01-19 20:04:52 -0600

    save changes

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,15 +1,42 @@
 import "./styles.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sendData = () => {
+    console.log(email, password);
+  };
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className="background">
       <div className="formContainer">
         <div className="form">
           <p className="title">Inicio</p>
           <div className="inputContainer">
-            <input className="input" placeholder="Email" />
-            <input className="input" placeholder="Password" />
+            <input
+              className="input"
+              placeholder="Email"
+              onChange={handleChangeEmail}
+            />
+            <input
+              className="input"
+              placeholder="Password"
+              onChange={handleChangePassword}
+            />
           </div>
+          <button className="iniciar" onClick={() => sendData()}>
+            Iniciar
+          </button>
           <div className="indications">
             <p className="text">¿No tienes una cuenta?</p>
             <button className="textRegister">Registrate</button>
